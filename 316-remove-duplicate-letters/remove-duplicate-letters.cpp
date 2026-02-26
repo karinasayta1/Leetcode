b98f9942f464bce96fc3115d4134f777a5e9a4e7
karinasayta1 <karinasayta85@gmail.com>
2026-02-26 21:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (46.65%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-       vector<int> last(26);
-        for(int i = 0; i < s.size(); i++) {
+       vector<int> last(26,0);
+       stack<char> st;
+       vector<bool> used(26, false);
+
+       for(int i = 0; i<s.size(); i++){
             last[s[i] - 'a'] = i;
-        }
-        
-        vector<bool> used(26, false);
-        stack<char> st;
-        
-        for(int i = 0; i < s.size(); i++) {
-            
+       }
+
+        for(int i = 0; i < s.size(); i++){
+
             char ch = s[i];
-            
             if(used[ch - 'a']) continue;
-            
-            while(!st.empty() &&
-                  ch < st.top() &&
-                  last[st.top() - 'a'] > i) {
-                
-                used[st.top() - 'a'] = false;
+
+            while(!st.empty() && ch<st.top()  && last[st.top()  - 'a'] > i){
+                used[st.top()-'a'] = false;
                 st.pop();
             }
-            
+
             st.push(ch);
-            used[ch - 'a'] = true;
+            used[st.top()-'a'] = true;  
         }
-        
-        string result = "";
-        
-        while(!st.empty()) {
-            result += st.top();
+
+        string ans;
+        while(!st.empty()){
+            ans += st.top();
             st.pop();
         }
-        
-        reverse(result.begin(), result.end());
-        return result;
 
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
